a5a3c43142c274b21f2a8316e88dc4eee8ceb492
Kyle Daniels <kyledaniels@Kyles-MacBook-Air.local>
2019-11-21 07:50:52 -0500

    Modified friend.js and package.json

---
+++ app/data/friend.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Michael Warner",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIgky2n_Bf-yD5dBEitnWP7qwqsBRHSc1cxaeLk-heGdUbnRMM&s",
+		scores: [
+			"5",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"4",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Oprah",
+		photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fa7%2Fbc%2Fd5%2Fa7bcd5d739b7a0fa5c027846d9eebdec.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F115967759124305578%2F&docid=cIlRYnlqSBoRmM&tbnid=cf131vsPjn5yWM%3A&vet=10ahUKEwjYu-v4tPrlAhUt11kKHcWUAXEQMwhiKBEwEQ..i&w=570&h=853&safe=active&bih=526&biw=960&q=celebrity%20headshot%20placeholder&ved=0ahUKEwjYu-v4tPrlAhUt11kKHcWUAXEQMwhiKBEwEQ&iact=mrc&uact=8",
+		scores: [
+			"3",
+			"1",
+			"5",
+			"4",
+			"1",
+			"5",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Kendrick Lamar",
+		photo: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fkpbs.media.clients.ellingtoncms.com%2Fimg%2Fphotos%2F2017%2F04%2F14%2Fkendrick.jpg&imgrefurl=https%3A%2F%2Fwww.kpbs.org%2Fnews%2F2018%2Fmay%2F11%2Fsan-diegos-top-weekend-events-spaces-and-places%2F&docid=KUo7sZ6_BEOJBM&tbnid=f1C2Zg9F8_80jM%3A&vet=10ahUKEwjYu-v4tPrlAhUt11kKHcWUAXEQMwilAShCMEI..i&w=1366&h=768&safe=active&bih=526&biw=960&q=celebrity%20headshot%20placeholder&ved=0ahUKEwjYu-v4tPrlAhUt11kKHcWUAXEQMwilAShCMEI&iact=mrc&uact=8",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;

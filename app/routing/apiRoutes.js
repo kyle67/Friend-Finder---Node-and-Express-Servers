f4febb0bd1a2d5dea00466ccb6968ec2678a86ec
Kyle Daniels <kyledaniels@Kyles-MacBook-Air.local>
2019-11-21 20:30:40 -0500

    Modified apiRoutes.js and server.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,7 @@
 //Load Data
 //Link routes to "data" sources.
 
-var friends= require('../data/friends.js')
+var friends= require('../data/friend.js')
 
 // Route apiRoutes with the app.get and the app.post functions
 
